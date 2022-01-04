6f454dd72283a5b8641e13bc083df28b4f11bdf1
John Lalor <john.lalor@nd.edu>
2022-01-04 13:23:14 -0500

    new cv

--- code/draft_picks.R
+++ code/draft_picks.R
@@ -3,6 +3,7 @@
 library(nflreadr)
 library(readr)
 library(dplyr)
+library(cfbfastR)
 
 D.draft <- load_draft_picks()
 
@@ -49,10 +50,45 @@ Ed Oliver
 Noah Fant
 "
 
+inout <- "
+Trey Adams, out
+Hunter Bryant, out
+Curtis Weaver, in
+Brandon Wimbush, out
+Jonathan Garvin, out
+Trevon Hill, out
+Jeff Thomas, out
+Amik Robertson, out
+Justin Madubuike, out
+Chuba Hubbard, in
+A.J. Epenesa, in
+Tristan Wirfs, in
+Mekhi Becton, out
+Cameron Dantzler, out
+CJ Henderson, out
+Brandon Aiyuk, out
+Cam Akers, out
+Jaylon Johnson, out
+Zack Moss, in
+Trevon Diggs, out
+Terrell Lewis, out
+Jerry Jeudy, in
+Alex Leatherwood, in
+Henry Ruggs III, in
+Kamal Martin, out
+Derrick Brown, in
+Tyler Biadasz, in
+Jonathan Taylor, in
+Justin Herbert, in
+Andrew Thomas, out
+D'Andre Swift, in
+AJ Dillon, out
+"
+
 
 sitoutDF <- read.csv(
-  text=sitouts,
-  col.names = c("names"),
+  text=inout,
+  col.names = c("names", "status"),
   header=F
 )
 
@@ -65,13 +101,25 @@ D.draft %>%
   print(n=40)
 
 
-D.season %>%
-  inner_join(
-    sitoutDF,
-    by = c("full_name" = "names")
+z <- D.season %>%
+  right_join(
+    mainData,
+    by = c("full_name" = "name")
   ) %>%
   arrange(full_name) %>%
-  print(n=40)
+  print(n=40) %>%
+  select(
+    full_name,
+    overall,
+    team,
+    nfl_team,
+    college_team,
+    status.x,
+    status.y,
+    position.x,
+    position.y
+    
+  )
 
 
 
@@ -79,5 +127,27 @@ D.season %>%
 
 # 1. Did their team win/lose their bowl game?
 # 2. Did comparable players (those drafted around the same time) win their bowl games?
+
+
+
+draft.2020 <- cfbd_draft_picks(year=2020)
+
+mainData <- draft.2020 %>%
+  right_join(
+    sitoutDF,
+    by = c("name" = "names")
+  ) %>%
+  select(
+    name,
+    overall,
+    nfl_team,
+    college_team,
+    status,
+    position
+  )
+
+# of those who were not drafted:
+# - 5 didnt play
+# - 3 did (all 3 false negatives)
 
 
